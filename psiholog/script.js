7c5509b62587f3108c4f1797e92bd1d033671355
wuruy <74461614+wuruy@users.noreply.github.com>
2020-11-24 20:33:07 +0200

    Add files via upload

---
+++ psiholog/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    PopUpHide();
+    PopUpHide2();
+    PopUpHide3();
+    PopUpHide4();
+    PopUpHide5();
+    PopUpHide6();
+    PopUpHide7();
+    PopUpHide8();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+
+function PopUpShow2(){
+    $("#popup2").show();
+}
+function PopUpHide2(){
+    $("#popup2").hide();
+}
+
+function PopUpShow3(){
+    $("#popup3").show();
+}
+function PopUpHide3(){
+    $("#popup3").hide();
+}
+
+function PopUpShow4(){
+    $("#popup4").show();
+}
+function PopUpHide4(){
+    $("#popup4").hide();
+}
+
+function PopUpShow5(){
+    $("#popup5").show();
+}
+function PopUpHide5(){
+    $("#popup5").hide();
+}
+
+function PopUpShow6(){
+    $("#popup6").show();
+}
+function PopUpHide6(){
+    $("#popup6").hide();
+}
+
+function PopUpShow7(){
+    $("#popup7").show();
+}
+function PopUpHide7(){
+    $("#popup7").hide();
+}
+
+function PopUpShow8(){
+    $("#popup8").show();
+}
+function PopUpHide8(){
+    $("#popup8").hide();
+}
