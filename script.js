7829b6d2a12cbac4941a06199955a15c2f502d5f
wuruy <74461614+wuruy@users.noreply.github.com>
2020-11-25 19:57:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+    PopUpHide();
+    PopUpHide2();
+    PopUpHide3();
+    PopUpHide4();
+    PopUpHide5();
+    PopUpHide6();
+    PopUpHide7();
+    PopImgHide1();
+    PopImgHide2();
+    PopImgHide3();
+    PopImgHide4();
+    PopImgHide5();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+
+function PopUpShow2(){
+    $("#popup2").show();
+}
+function PopUpHide2(){
+    $("#popup2").hide();
+}
+
+function PopUpShow3(){
+    $("#popup3").show();
+}
+function PopUpHide3(){
+    $("#popup3").hide();
+}
+
+function PopUpShow4(){
+    $("#popup4").show();
+}
+function PopUpHide4(){
+    $("#popup4").hide();
+}
+
+function PopUpShow5(){
+    $("#popup5").show();
+}
+function PopUpHide5(){
+    $("#popup5").hide();
+}
+
+function PopUpShow6(){
+    $("#popup6").show();
+}
+function PopUpHide6(){
+    $("#popup6").hide();
+}
+
+function PopUpShow7(){
+    $("#popup7").show();
+}
+function PopUpHide7(){
+    $("#popup7").hide();
+}
+
+
+function PopImgShow1(){
+    $("#popupImg1").show();
+}
+function PopImgHide1(){
+    $("#popupImg1").hide();
+}
+function PopImgShow2(){
+    $("#popupImg2").show();
+}
+function PopImgHide2(){
+    $("#popupImg2").hide();
+}
+function PopImgShow3(){
+    $("#popupImg3").show();
+}
+function PopImgHide3(){
+    $("#popupImg3").hide();
+}
+function PopImgShow4(){
+    $("#popupImg4").show();
+}
+function PopImgHide4(){
+    $("#popupImg4").hide();
+}
+function PopImgShow5(){
+    $("#popupImg5").show();
+}
+function PopImgHide5(){
+    $("#popupImg5").hide();
+}
